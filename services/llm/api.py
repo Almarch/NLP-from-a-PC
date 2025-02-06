d05cd4459546713b8576f369c3639c761727a3dc
Almarch <alexandre.marchal@hotmail.com>
2025-02-06 09:02:55 +0100

    make ntokens a parameter

--- services/llm/api.py
+++ services/llm/api.py
@@ -30,11 +30,21 @@ class Message(BaseModel):
     content: str
 class RequestBody(BaseModel):
     text: List[Message]
+    ntokens: int
 
 @app.post("/hey")
 async def generate(request: RequestBody):
-    text = request.text
-    input_tensor = tokenizer.apply_chat_template(text, add_generation_prompt=True, return_tensors="pt")
-    outputs = model.generate(input_tensor.to(model.device), max_new_tokens=100)
-    result = tokenizer.decode(outputs[0][input_tensor.shape[1]:], skip_special_tokens=True)
+    input_tensor = tokenizer.apply_chat_template(
+        request.text,
+        add_generation_prompt=True,
+        return_tensors="pt"
+    )
+    outputs = model.generate(
+        input_tensor.to(model.device),
+        max_new_tokens=request.ntokens
+    )
+    result = tokenizer.decode(
+        outputs[0][input_tensor.shape[1]:],
+        skip_special_tokens=True
+    )
     return result
