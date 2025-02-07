52340627505668caecf0881a3b8ab46c83e4c427
Almarch <alexandre.marchal@hotmail.com>
2025-02-07 18:22:50 +0100

    structuration: include OCR

---
+++ services/ocr/api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import pytesseract
+from PIL import Image
+import io
+
+app = FastAPI()
+
+@app.post("/ocr/")
+async def extract_text(file: UploadFile = File(...)):
+    image = Image.open(io.BytesIO(await file.read()))
+    text = pytesseract.image_to_string(image)
+    return {"filename": file.filename, "text": text}
+
